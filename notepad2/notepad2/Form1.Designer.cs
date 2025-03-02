13b6b822a41f041ab88b399a7789739147018223
Filip Švehla <145663563+Lachtanecicek@users.noreply.github.com>
2025-03-02 13:04:00 +0100

    Add files via upload

---
+++ notepad2/notepad2/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace notepad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.souborToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novýToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otevřítToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uložitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.konecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nástrojeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.písmoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.barvaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pozadíToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(1, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1004, 459);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.souborToolStripMenuItem,
+            this.nástrojeToolStripMenuItem,
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1009, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // souborToolStripMenuItem
+            // 
+            this.souborToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novýToolStripMenuItem,
+            this.otevřítToolStripMenuItem,
+            this.uložitToolStripMenuItem,
+            this.konecToolStripMenuItem});
+            this.souborToolStripMenuItem.Name = "souborToolStripMenuItem";
+            this.souborToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.souborToolStripMenuItem.Text = "Soubor";
+            // 
+            // novýToolStripMenuItem
+            // 
+            this.novýToolStripMenuItem.Name = "novýToolStripMenuItem";
+            this.novýToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.novýToolStripMenuItem.Text = "Nový";
+            // 
+            // otevřítToolStripMenuItem
+            // 
+            this.otevřítToolStripMenuItem.Name = "otevřítToolStripMenuItem";
+            this.otevřítToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.otevřítToolStripMenuItem.Text = "Otevřít";
+            this.otevřítToolStripMenuItem.Click += new System.EventHandler(this.otevřítToolStripMenuItem_Click);
+            // 
+            // uložitToolStripMenuItem
+            // 
+            this.uložitToolStripMenuItem.Name = "uložitToolStripMenuItem";
+            this.uložitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uložitToolStripMenuItem.Text = "Uložit";
+            this.uložitToolStripMenuItem.Click += new System.EventHandler(this.uložitToolStripMenuItem_Click);
+            // 
+            // konecToolStripMenuItem
+            // 
+            this.konecToolStripMenuItem.Name = "konecToolStripMenuItem";
+            this.konecToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.konecToolStripMenuItem.Text = "Konec";
+            this.konecToolStripMenuItem.Click += new System.EventHandler(this.konecToolStripMenuItem_Click_2);
+            // 
+            // nástrojeToolStripMenuItem
+            // 
+            this.nástrojeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.písmoToolStripMenuItem,
+            this.barvaToolStripMenuItem,
+            this.pozadíToolStripMenuItem});
+            this.nástrojeToolStripMenuItem.Name = "nástrojeToolStripMenuItem";
+            this.nástrojeToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.nástrojeToolStripMenuItem.Text = "Nástroje";
+            // 
+            // písmoToolStripMenuItem
+            // 
+            this.písmoToolStripMenuItem.Name = "písmoToolStripMenuItem";
+            this.písmoToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.písmoToolStripMenuItem.Text = "Písmo";
+            this.písmoToolStripMenuItem.Click += new System.EventHandler(this.písmoToolStripMenuItem_Click);
+            // 
+            // barvaToolStripMenuItem
+            // 
+            this.barvaToolStripMenuItem.Name = "barvaToolStripMenuItem";
+            this.barvaToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.barvaToolStripMenuItem.Text = "Barva písma";
+            this.barvaToolStripMenuItem.Click += new System.EventHandler(this.barvaToolStripMenuItem_Click);
+            // 
+            // pozadíToolStripMenuItem
+            // 
+            this.pozadíToolStripMenuItem.Name = "pozadíToolStripMenuItem";
+            this.pozadíToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.pozadíToolStripMenuItem.Text = "Pozadí";
+            this.pozadíToolStripMenuItem.Click += new System.EventHandler(this.pozadíToolStripMenuItem_Click);
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.oProgramuToolStripMenuItem.Text = "O programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 488);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Nejlepší textový program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem souborToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nástrojeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novýToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otevřítToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uložitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem konecToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem písmoToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem barvaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pozadíToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ notepad2/notepad2/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad2
+{
+    public partial class Form1 : Form
+    {
+        string soubor = "☼♫♀";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oProgramuToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Notepad v2.0\nFilip Švehla\n2022",
+                "Notepad v2.0",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void konecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void konecToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void konecToolStripMenuItem_Click_2(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Opravdu chcete program ukončit?",
+                "Konec",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void písmoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+                richTextBox1.Font = fontDialog1.Font;
+        }
+
+        private void písmoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void barvaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                richTextBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private void pozadíToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+                richTextBox1.BackColor = colorDialog2.Color;
+        }
+
+        private void otevřítToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Textové soubory|*.txt|Všechny soubory|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+                soubor = openFileDialog1.FileName;
+            }
+        }
+
+        private void uložitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.FileName != soubor)
+            {
+                saveFileDialog1.Filter = "Textové soubory|*.txt|Všechny soubory|*.*";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // Uložit jako...
+                    StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                    sw.Write(richTextBox1.Text);
+                    sw.Close();
+                    soubor = saveFileDialog1.FileName;
+                }
+            }
+            else
+            {
+                // Uložit
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                sw.Write(richTextBox1);
+                sw.Close();
+                soubor = openFileDialog1.FileName;
+            }
+        }
+    }
+}
